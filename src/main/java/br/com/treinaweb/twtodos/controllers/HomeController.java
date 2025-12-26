3573860c14776e9785d1caf1cd7cfdce015c06f7
Diego Jardim <diegomeira2812@gmail.com>
2025-12-25 23:41:50 -0300

    feat
    add entidade Todo

--- src/main/java/br/com/treinaweb/twtodos/controllers/HomeController.java
+++ src/main/java/br/com/treinaweb/twtodos/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package br.com.treinaweb.twtodos.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
     
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String home() {
         return "home";
     }
